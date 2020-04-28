cfe913024b63c99f7c47a6362930797ecb1a9400
Ruma Das <rumaadl16@gmail.com>
2020-04-29 01:44:40 +0930

    class declear

--- app.js
+++ app.js
@@ -4,15 +4,49 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const axios = require("axios");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter name of team member"
+        },
+        {
+            type: "input",
+            name: "role",
+            message: "Enter role of team member"
+        },
+        {
+            type: "input",
+            name: "Id",
+            message: "Enter Tem Member ID"
+        },
+        {
+            type: "input",
+            name: "GitHubUser",
+            message: "Enter  GitHub Username"
+        },
+    ]
+
+    )
+    .then(function ({ GitHubUser }) {
+        const queryUrl = `https://api.github.com/users/${GitHubUser}?client_id=b77dd6d5ba39bf8bca34&client_secret=8a78678b6f246d35c590f5f6088859266fc6b0d2`;
+        axios.get(queryUrl).then(function (res) {
+            console.log(res.data);
+            console.log(res.data.email);
+        });
+    });
+
 // and to create objects for each team member (using the correct classes as blueprints!)
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
